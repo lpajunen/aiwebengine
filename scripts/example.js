cba6d3d84e5e2954dff658b324581a02147e859e
Lasse Pajunen <lpajunen@gmail.com>
2025-09-11 20:29:27 +0300

    clean up

--- scripts/example.js
+++
@@ -1,6 +0,0 @@
-// Example QuickJS script to register routes
-// register('/hello', (req) => ({ status: 200, body: 'Hello from JS!' }));
-
-// register a sample route
-function example_hello(req) { return { status: 200, body: `Hello from JS! method=${req.method}` }; }
-register('/hello', 'example_hello');
