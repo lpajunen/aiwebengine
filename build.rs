1a4bb51df1fa28747dda525158843e7ae61846c2
Lasse Pajunen <lpajunen@gmail.com>
2026-01-18 12:29:28 +0200

    upgrade vergen

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
 //! Build script for capturing build metadata at compile time
 //!
-//! This script uses vergen to capture git commit information and build timestamps
+//! This script uses vergen-gix to capture git commit information and build timestamps
 //! that will be embedded in the binary. If git is unavailable (e.g., building from
 //! a source tarball or in Docker without .git), it will use environment variables
 //! passed during build (VERGEN_GIT_SHA, VERGEN_GIT_COMMIT_TIMESTAMP, etc.)
 
-use vergen::EmitBuilder;
+use std::error::Error;
+use vergen_gix::{Build, Emitter, Gix};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     // Check if git metadata is already provided via environment variables
     // This is the case in Docker builds where .git is not available
     let has_env_metadata = std::env::var("VERGEN_GIT_SHA").is_ok()
@@ -19,11 +20,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("cargo:warning=Using git metadata from environment variables");
         Ok(())
     } else {
-        // Configure vergen to emit git and build information
+        // Configure vergen-gix to emit git and build information
         // If git is not available, vergen will set empty values
-        EmitBuilder::builder()
-            .all_git()
-            .all_build()
+        let build = Build::all_build();
+        let gix = Gix::all_git();
+        Emitter::default()
+            .add_instructions(&build)?
+            .add_instructions(&gix)?
             .emit()?;
         Ok(())
     }
