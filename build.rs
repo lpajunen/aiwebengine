0938a872403a7fdbeb37dc68d4db9803e7920402
Lasse Pajunen <lpajunen@gmail.com>
2026-01-18 10:46:15 +0200

    improve version info

--- build.rs
+++ build.rs
@@ -22,9 +22,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Configure vergen to emit git and build information
         // If git is not available, vergen will set empty values
         EmitBuilder::builder()
-            .git_sha(true)          // Git commit hash (short form)
-            .git_commit_timestamp() // Git commit timestamp (ISO 8601)
-            .build_timestamp()      // Build timestamp (ISO 8601)
+            .all_git()
+            .all_build()
             .emit()?;
         Ok(())
     }
